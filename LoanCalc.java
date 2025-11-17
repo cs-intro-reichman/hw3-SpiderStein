eaddf4b8299ebb644651d83cc22583d06174acfd
Idan Steinberg <steinberg.idan@gmail.com>
2025-11-17 10:36:03 +0200

    LoanCalc RC

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,25 +1,22 @@
 // Computes the periodical payment necessary to pay a given loan.
 public class LoanCalc {
 
-	static double epsilon = 0.001; // Approximation accuracy
-	static int iterationCounter; // Number of iterations
+	static double epsilon = 0.001;
+	static int iterationCounter;
 
 	// Gets the loan data and computes the periodical payment.
 	// Expects to get three command-line arguments: loan amount (double),
 	// interest rate (double, as a percentage), and number of payments (int).
 	public static void main(String[] args) {
-		// Gets the loan data
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
-		// Computes the periodical payment using brute force search
 		System.out.print("\nPeriodical payment, using brute force: ");
 		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
 
-		// Computes the periodical payment using bisection search
 		System.out.print("\nPeriodical payment, using bi-section search: ");
 		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
